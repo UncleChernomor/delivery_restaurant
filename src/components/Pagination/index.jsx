1a73da724fbbe972aa53090928f25f5e8c03ce4e
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-06-08 23:57:23 +0300

    add app can get count page using query from mockapi

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,29 +1,44 @@
-import React, { useState } from 'react';
+import { useState, useSyncExternalStore } from 'react';
 import styles from './Pagination.module.scss';
 
-function Pagination({ countPage, ...props }) {
-    const [sizePagination, setSizePagination] = useState(new Array(countPage));
+function Pagination({ countPage, setPage, ...props }) {
+    const [isActive, setIsActive] = useState(0);
 
-    console.log('countPage: ' + countPage);
-    console.log(sizePagination.length);
+    /**
+     * 
+     * @param {*} type arrow 'left' or 'rigth'
+     */
+    const setIndexActivePage = (type) => {
+        if (type === 'left' && isActive > 0) { setIsActive(isActive - 1); setPage(isActive - 1); }
+        else if (type === 'rigth' && isActive < (countPage - 1)) { setIsActive(isActive + 1); setPage(isActive + 1); }
+        console.log('1- isActive: ' + isActive)
+        //setPage(isActive);
+    }
+
     return (
-        <div>
-            <ul className={styles.root}>
-                <li className={styles.item_icon}>
+        <div className={styles.root}>
+            <ul>
+                <li className={styles.item_icon} onClick={() => setIndexActivePage('left')}>
                     <svg className={styles.icon} viewBox="0 0 32 32">
                         <path d="M31,16A15,15,0,1,1,16,1,15,15,0,0,1,31,16ZM3,16A13,13,0,1,0,16,3,13,13,0,0,0,3,16Z" />
                         <path d="M19.87,10.41,14.29,16l5.58,5.59a1,1,0,0,1,0,1.41h0a1,1,0,0,1-1.41,0L12.1,16.64a.91.91,0,0,1,0-1.28L18.46,9a1,1,0,0,1,1.41,0h0A1,1,0,0,1,19.87,10.41Z" />
                     </svg></li>
-                {/* This is the page adding loop here*/}
+                {/* This is the number page adding loop here*/}
                 {
-                    [...sizePagination].map((_, index) => <li className={styles.item} key={index}>{index + 1}</li>)
+                    Array.from({ length: countPage }, (_, index) => {
+                        return (<li className={isActive === index ? styles.pselected : ''}
+                            key={index} onClick={() => {
+                                setPage(index);
+                                setIsActive(index);
+                            }}>{index + 1}
+                        </li>)
+                    })
                 }
-
 
                 {/* <li className={styles.item} onClick={(e) => e.target.classList.add('active')}>1</li>
                 <li className={styles.item}>2</li>
                 <li className={styles.item}>3</li> */}
-                <li className={styles.item_icon}>
+                <li className={styles.item_icon} onClick={() => setIndexActivePage('rigth')}>
                     <svg className={styles.icon} viewBox="0 0 32 32">
                         <path d="M1,16A15,15,0,1,1,16,31,15,15,0,0,1,1,16Zm28,0A13,13,0,1,0,16,29,13,13,0,0,0,29,16Z" />
                         <path d="M12.13,21.59,17.71,16l-5.58-5.59a1,1,0,0,1,0-1.41h0a1,1,0,0,1,1.41,0l6.36,6.36a.91.91,0,0,1,0,1.28L13.54,23a1,1,0,0,1-1.41,0h0A1,1,0,0,1,12.13,21.59Z" />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import { SearchContext } from '../App';
 import Categories from '../components/Categories';
@@ -8,36 +8,61 @@ import Skeleton from '../components/Skeleton';
 import filterData from '../utils/search.mjs';
 import Pagination from '../components/Pagination';
 
-
 function Home(props) {
+    const { searchValue } = useContext(SearchContext);
+
     const [items, setItems] = useState(new Array(6));
     const [isLoading, setIsLoading] = useState(true);
     const [activeCategory, setActiveCategory] = useState(0);
     const [activeItemSorting, setActiveItemSorting] = useState(0);
-    const { searchValue } = useContext(SearchContext);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [countPage, setCountPage] = useState(0);
 
     const sortName = ['rating', 'price', 'title'];
+    const countItemOnPage = 4;
+    let countAllItems = 10;
+
 
     useEffect(() => {
         // console.log(activeCategory);
         setIsLoading(true);
+        setCountPage(Math.ceil(countAllItems / countItemOnPage));
+
+        //включаем в строку всегда, но после первого запроса
+        let strSort = `?sortBy=${sortName[activeItemSorting]}`;
+        //включаем после первого запроса, после которого пагинацию делаем
+        let strPage = `&page=${countPage ? (currentPage + 1) : ''}&limit=${countPage ? countItemOnPage : ''}`;
+        //можно либо категорию использовать, либо строку поиска. Включаем при категории >0 
+        let strCategory = activeCategory ? `&category=${activeCategory}` : '';
+        //Включаем как есть strSearch
+        let strSearch = searchValue ? `&title=${searchValue}` : '';
+        const strQuery = `https://629603be810c00c1cb6d58ed.mockapi.io/items${strCategory}${strSearch}`;
+        console.log('strQuery: ' + strQuery);
 
         // use mockapi server for test data
-        fetch(`https://629603be810c00c1cb6d58ed.mockapi.io/items?category=${activeCategory ? activeCategory : ''}&sortBy=${sortName[activeItemSorting]}`)
-            .then((response) => {
-                return response.json();
+        fetch(strQuery)
+            .then((response) => response.json())
+            .then(data => {
+                return new Promise((resolve, reject) => {
+                    console.log(data.length);
+                    setCountPage(Math.ceil(data.length / countItemOnPage));
+                    console.log(countPage);
+                    strPage = `&page=${Math.ceil(data.length / countItemOnPage) ? (currentPage + 1) : ''}&limit=${Math.ceil(data.length / countItemOnPage) ? countItemOnPage : ''}`;
+                    resolve(0);
+                })
             })
+            .then(() => fetch(strQuery + strSort + strPage))
+            .then(response => response.json())
             .then((data) => {
                 setItems(data);
                 setIsLoading(false);
-                if (searchValue.length > 0) setItems(filterData(items, searchValue));
             })
             .catch(error => {
                 console.log(error);
             });
 
         // window.scrollTo(0, 0);
-    }, [activeCategory, activeItemSorting, searchValue]);
+    }, [activeCategory, activeItemSorting, searchValue, currentPage]);
 
     /**
      * callback for set active category
@@ -47,8 +72,32 @@ function Home(props) {
 
     const changeSorting = (i) => { setActiveItemSorting(i); }
 
-    console.log('item-- ' + items.length);
-    console.log('item-- ' + items.length / 4);
+    /**
+     * callback for Pagination
+     * @param {*} index current page
+     */
+    const showPage = (index) => {
+        setCurrentPage(index);
+    }
+
+    async function countCurrentItems(strQuery) {
+        let result = 0;
+
+        await fetch(strQuery)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                result = data.length
+                setCountPage(Math.ceil(result / countItemOnPage));
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+        return result;
+    }
+
     return (
         <>
             <div className="content__top">
@@ -62,7 +111,7 @@ function Home(props) {
                         : [...items].map((_, index) => (<Skeleton key={index} />))
                 }
             </div>
-            <Pagination countPage={Math.ceil(items.length / 4)} />
+            <Pagination countPage={countPage} setPage={showPage} />
         </>
     );
 }
