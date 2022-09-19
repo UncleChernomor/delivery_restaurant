424dfb906fa4f2c584dd3ed208e0360823170e47
UncleChernomor <96253164+UncleChernomor@users.noreply.github.com>
2022-09-19 19:52:42 +0300

    add types cartSlice

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -9,4 +9,7 @@ export const store = configureStore({
         cart,
         products,
     },
-})
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
